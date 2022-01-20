c0d303595eb4d1e9d81be60892de718f6b5d34d7
Shayan Shojaei <shayanshojaei.2002@gmail.com>
2022-01-20 22:45:16 +0330

    feat: create initial project structure

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './index.module.scss';
+
+type Props =
+React.DetailedHTMLProps<
+React.ButtonHTMLAttributes<HTMLButtonElement>,
+HTMLButtonElement
+>
+& { title:string };
+
+/* eslint-disable react/jsx-props-no-spreading */
+function Button({ title, ...props }:Props) {
+  return <button
+    className={styles.button}
+    type="button" {...props}>{title}</button>;
+}
+export default Button

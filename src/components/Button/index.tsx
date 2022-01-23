920d734f229c4f414aae5d513c0e5c50a9bcf223
Shayan Shojaei <shayanshojaei.2002@gmail.com>
2022-01-23 22:24:42 +0330

    feat: create button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,17 +1,32 @@
-import React from 'react';
-import styles from './index.module.scss';
+import cn from "classnames";
+import React from "react";
+import styles from "./index.module.scss";
+
+enum ButtonType {
+  Primary="primary", Secondary="secondary",
+  PrimaryLarge="primaryLarge", SecondaryLarge="secondaryLarge"
+}
 
 type Props =
 React.DetailedHTMLProps<
 React.ButtonHTMLAttributes<HTMLButtonElement>,
 HTMLButtonElement
 >
-& { title:string };
+& {
+  title:string,
+  buttonType?:ButtonType|string
+};
 
 /* eslint-disable react/jsx-props-no-spreading */
-function Button({title, ...props}:Props) {
+function Button({title,buttonType, ...props}:Props) {
   return <button
-    className={styles.button}
+    className={cn({
+      [styles.button]:true,
+      [styles.primary]: buttonType === undefined || buttonType === ButtonType.Primary,
+      [styles.primaryLarge]: buttonType === ButtonType.PrimaryLarge,
+      [styles.secondary]: buttonType === ButtonType.Secondary,
+      [styles.secondaryLarge]: buttonType === ButtonType.SecondaryLarge,
+    })}
     type="button" {...props}>{title}</button>;
 }
 export default Button
---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,35 @@
+import {ComponentMeta, ComponentStory} from "@storybook/react";
+import React from "react";
+import Button from "../components/Button";
+
+
+export default {
+  title: "Button",
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args}/>;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  buttonType: "primary",
+  title:"Primary Button"
+};
+
+export const PrimaryLarge = Template.bind({});
+PrimaryLarge.args = {
+  buttonType: "primaryLarge",
+  title:"Primary Large Button"
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  buttonType: "secondary",
+  title:"Secondary Button"
+};
+
+export const SecondaryLarge = Template.bind({});
+SecondaryLarge.args = {
+  buttonType: "secondaryLarge",
+  title:"Secondary Large Button"
+};
